3034362b92664db0590f9f3cc4a3dc7d821c91b0
Andrei <65533264+Fennec-coder@users.noreply.github.com>
2021-08-02 13:21:45 +0300

    implement distance request and price calculation

--- cargo.rb
+++ cargo.rb
@@ -1,17 +1,61 @@
 # frozen_string_literal: true
 
-class Cargo
-  @distance = 0
-  @price = 0
+require 'net/http'
+require 'json'
 
-  def initialize(weight, length, height)
+class Cargo
+  SOURCE = 'https://api.mapbox.com/directions-matrix/v1/mapbox/driving/'
+
+  def initialize(weight, length, height, width, origin_location, destination)
     @weight = weight
     @length = length
+    @width = width
     @height = height
+    @origin_location = origin_location
+    @destination = destination
+    @path = "#{SOURCE}#{origin_location.to_s.gsub(' ', '')};#{destination.to_s.gsub(' ', '')}"
   end
 
-  # @return [Hash{Symbol->Unknown}]
-  def get_hash
-    { weight: @weight, length: @length, height: @height, distance: @distance, price: @price }
+  # @return [Hash]
+  def hash
+    { weight: @weight, length: @length, width: @width, height: @height, distance: calculate_distance, price: calculate_price }
+  end
+
+  # @return [integer]
+  def rate
+    cubic_meter = @length * @width * @height
+    if cubic_meter > 1
+      if @weight > 10
+        3
+      else
+        2
+      end
+    else
+      1
+    end
+  end
+
+  # @return [string]
+  def calculate_distance
+    uri = URI(@path)
+
+    params = {
+      access_token: 'pk.eyJ1IjoiZmVubmVjLWNvZGVyIiwiYSI6ImNrcnRudmdzODFpcGYydnBmOGpna2htYXgifQ.GvgVdsIEcDTvmAyvi4p6MA',
+      sources: 0,
+      annotations: 'distance'
+    }
+
+    uri.query = URI.encode_www_form(params)
+    res = Net::HTTP.get_response(uri)
+    answer = res.body if res.is_a?(Net::HTTPSuccess)
+    JSON.parse(answer)['distances'][0][1].to_i
+  end
+
+  def calculate_price
+    rate * (calculate_distance.to_i / 100)
   end
 end
+
+# create obj:
+# Cargo.new(11, 100, 2, 2,
+#           '44.60209193847771, 33.525387160183136', '44.605040492759855,33.52721106226844')
